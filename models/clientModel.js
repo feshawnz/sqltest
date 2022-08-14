a73e6051309c9d7ddd7f98c6245e558fb9fd15ff
Shawn Zhou <feshawnzhou@gmail.com>
2022-08-13 20:05:23 -0700

    update

--- models/clientModel.js
+++ models/clientModel.js
@@ -8,26 +8,22 @@ var clientModel = {
 	all: function (cb) {     
 		orm.all('clients', function (res) {
 			cb(res);
-			console.log("orm model ALL is working")
 		});
 	},
 	// cols and vals are arrays
 	create: function (column, values, cb) {
 		orm.create('clients',column,values, function (res) {
 			cb(res);
-			console.log("orm model CREATE is working")
 		});
 	},
 	update: function (column, newValue, condition, cb) {
 		orm.update('clients', column, newValue, condition, function (res) {
 			cb(res);
-			console.log("orm model UPDATE is working")
 		});
 	},
 	delete: function (condition, cb) {
 		orm.delete('clients',condition, function (res) {
 			cb(res);
-			console.log("orm model DELELTE is working")
 		});
 	}
 };
--- models/sectionsModel.js
+++ models/sectionsModel.js
@@ -8,26 +8,22 @@ var sectionsModel = {
 	all: function (cb) {     
 		sectionsOrm.all('sections', function (res) {
 			cb(res);
-			console.log("sectionsOrm model ALL is working")
 		});
 	},
 	// cols and vals are arrays
 	create: function (column1,column2, values, id, cb) {
 		sectionsOrm.create('sections',column1, column2,values,id, function (res) {
 			cb(res);
-			console.log("sectionsOrm model CREATE is working")
 		});
 	},
 	update: function (column, newValue, condition, cb) {
 		sectionsOrm.update('sections', column, newValue, condition, function (res) {
 			cb(res);
-			console.log("sectionsOrm model UPDATE is working")
 		});
 	},
 	delete: function (condition, cb) {
 		sectionsOrm.delete('sections',condition, function (res) {
 			cb(res);
-			console.log("sectionsOrm model DELELTE is working")
 		});
 	}
 };
